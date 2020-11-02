98c1c70c671cac51318323552101adb1aa4193ec
bejarane <bejarane.e@gmail.com>
2020-11-01 21:54:49 -0600

    finalizado, entrega final

--- rodemmanuel.cpp
+++ rodemmanuel.cpp
@@ -1,3 +1,17 @@
+///////// 2020. GNU GENERAL PUBLIC LICENSE /////////////////////////
+//
+//  Project     : Prueba de diferentes algoritmos de ordenamiento
+//  File        : rodemmanuel.cpp
+//  Description :
+//      Archivo principal. Codigo para cada método de búsqueda y 
+//              funcion de llamada inicial.
+//
+//  Authors     : E. Rodriguez
+//
+//  Git repository: https://github.com/bejarane/CA3125_sorting
+////////////////////////////////////////////////////////////////////
+
+
 #include <bits/stdc++.h>
 using namespace std;
 /**
@@ -71,15 +85,19 @@ vector <unsigned int> insertion (vector <unsigned int> data){
  * @return Un vector ordenado
 */
 vector <unsigned int> quick (vector <unsigned int> data, int bottom, int top){
+    //vPrint(data);
+    //cout << "ping1,"<<bottom<<" "<<top<<"\n";
     if ( (top-bottom) < 2){ //caso final, 1 o 0 casillas por ordenar
-        if (data[top] < data[bottom]){
+        if (data[top] < data[bottom] && top>bottom){
             data[bottom]^=data[top];
             data[top]^=data[bottom];
             data[bottom]^=data[top]; //intercambio bitwise
         }
         return data;
     }
+    //cout << "ping1.5\n";
     unsigned int valP = data[top];
+    //cout << "ping2\n";
     int lower = bottom, upper = top-1;
     while(lower!=upper+1){
         if (data[lower]>valP){
@@ -183,12 +201,39 @@ vector <unsigned int> mergeSort (vector <unsigned int> data){
 }
 
 
+vector <unsigned int> counter (vector<unsigned int> data, int total, int exp){
+    vector <unsigned int> output = data;
+    vector <int> counter (10,0);
+    int i;
+    for (i =0; i<total; i++)
+        counter[(data[i]/exp)%10]++;
+
+    for (i =1; i<10; i++)
+        counter[i]+=counter[i-1];
+
+    for (i = total-1; i>=0; i--){
+        output[counter[(data[i]/exp)%10]-1]=data[i];
+        counter[(data[i]/exp)%10]--;
+    }
+
+    return output;     
+}
+
 
 /**
- * Funcion para ordenamiento por bubblesort
+ * Funcion para ordenamiento por radix sort
  * @param data Se trata de un vector con los datos a ordenar
  * @return Un vector ordenado
 */
+vector <unsigned int> radix(vector <unsigned int> data){
+    int total = data.size();
+    unsigned int maxN = *max_element(data.begin(),data.end());
+
+    for (int exp = 1; maxN / exp > 0; exp *=10)
+       data = counter(data,total, exp);
+
+    return data;
+}
 
 vector<unsigned int> randomGen(int b){
     vector <unsigned int> resultado;
@@ -227,7 +272,7 @@ long int evaluator(vector <unsigned int> data, int cycles,vector<unsigned int>(*
     }
     end_time = std::chrono::high_resolution_clock::now();
 
-    vPrint(result);
+    //vPrint(result);
 
     return std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
 }
@@ -239,20 +284,21 @@ int main(){
     vector <long int> times;
     const vector <string> tests = {"Bubble Sort","Insertion Sort","Quick Sort","Merge Sort","Radix Sort"};
 
-    //cin >> a >> b;
-    a = 1;
-    b = 5;
+    //a = 1;
+    //b = 2500;
+    cin >> a >> b;
 
-    //vector <unsigned int> test = {0,8,5,6,7,1,2,8,6,5,12,585,12,11,13,15};
+    //vector <unsigned int> test = {176311416, 597941521, 56451127, 716072915, 830816027};
     vector<unsigned int> test = randomGen(b);
 
-    vPrint(test);
+    //vPrint(test);
     vector <unsigned int> result;
 
     times.push_back(evaluator(test,a,bubble));
     times.push_back(evaluator(test,a,insertion));
     times.push_back(evaluator(test,a,quick));
     times.push_back(evaluator(test,a,mergeSort));
+    times.push_back(evaluator(test,a,radix));
 
 
     cout<< "\nResultados\n";
